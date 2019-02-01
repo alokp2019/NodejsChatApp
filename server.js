979063bf99932afc1ae9c8d697811bf77e3a28d5
ALOK P <alokp2019@gmail.com>
2019-02-02 01:22:44 +0530

    server initialized

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res,next)=>{
+   
+   res.send('hello');
+
+});
+
+app.listen(3000, ()=>{
+	console.log('hello buddy');
+})
